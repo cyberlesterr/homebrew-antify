47132d66fc0dd551c9afa92ab6c5cca13ad1835c
Lester <leasingzong@gmail.com>
2026-07-14 23:54:43 +0800

    Update antify to 0.0.56

--- Casks/antify.rb
+++ Casks/antify.rb
@@ -1,6 +1,6 @@
 cask "antify" do
-  version "0.0.55"
-  sha256 "93e42d9630fe98e7e547fd1a475f6e1d5addf6d7d4f99ca5bd1938501af9bf6a"
+  version "0.0.56"
+  sha256 "025788e86690a2e428a9ec674911fec0c1b5c4f4c31e5f2df017f3ab3c84c1b0"
 
   url "https://r2.antifyapp.com/releases/v#{version}/Antify.dmg"
   name "Antify"
